acf2e08a74bb37aef657c7cdd5b75eb6a0b4296b
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-10-07 23:55:18 +0500

    Task completed

--- index.js
+++ index.js
@@ -3,25 +3,25 @@
 var phoneBook = require('./phoneBook');
 
 // Добавляем записи
-phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
+phoneBook.add('Сергей', '7 999 6667778', 'Gs@example.com');
 phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
 phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
 
 // Невалидные данные не должны попадать в книгу!
-phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
+phoneBook.add('Честный Хрюндель', 'invalid phone777', 'honest-hrundel');
 
-phoneBook.find('777');
+// phoneBook.find('777');
 // Выводит построчно записи, все поля через запятую:
 // Сергей, +7 (999) 666-7-778, gogolef@yandex-team.ru
 // Олег, +7 (999) 777-7-777, just7@yandex-team.ru
 
-phoneBook.remove('Олег');
-// Выводит количество удалённых контактов, которые удовлетворят запросу:
-// Удален 1 контакт
+// phoneBook.remove('Олег');
+// // Выводит количество удалённых контактов, которые удовлетворят запросу:
+// // Удален 1 контакт
 
-// Выводит записи в виде красивой таблички
-phoneBook.showTable();
-// Выводит
+// // Выводит записи в виде красивой таблички
+// phoneBook.showTable();
+// // Выводит
 // ┌─────────────┬────────────────────╥──────────────────┐
 // │ Имя         │ Телефон            ║ email            │
 // ├─────────────┼────────────────────╫──────────────────┤
@@ -30,6 +30,11 @@ phoneBook.showTable();
 // └─────────────┴────────────────────╨──────────────────┘
 
 
-// Экспортируем записи, пример файла рядом
-phoneBook.export('./backup.csv');
-// Добавлено 4 контакта
+// // Экспортируем записи, пример файла рядом
+phoneBook.importFromCsv('backup.csv');
+// phoneBook.find('m');
+// phoneBook.remove('777');
+// phoneBook.find('');
+// // Добавлено 4 контакта
+
+phoneBook.showTable();
--- phoneBook.js
+++ phoneBook.js
@@ -1,52 +1,193 @@
 'use strict';
+Object.values = obj => Object.keys(obj).map(key => obj[key]);
+var phoneRegExp = /^(\+?\d{1,2})?\s?(\(\d{3}\)|\d{3})\s?\d{3}(\-\d\-|\s\d\s|\d)\d{3}$/;
+var emailRegExp = /^([a-z0-9._-])*@([a-zа-я0-9_-]+\.)+([a-zа-я]+)$/;
+var phoneBook = {};
 
-var phoneBook; // Здесь вы храните записи как хотите
+function isPhoneValid(phone) {
+    return phoneRegExp.test(phone);
+}
 
-/*
-   Функция добавления записи в телефонную книгу.
-   На вход может прийти что угодно, будьте осторожны.
-*/
+function isEmailValid(email) {
+
+    return emailRegExp.test(email);
+}
+
 module.exports.add = function add(name, phone, email) {
-
-    // Ваша невероятная магия здесь
-
+    if (!(typeof name === 'string' && typeof phone === 'string' && typeof email === 'string')) {
+        return;
+    }
+    var lowerEmail = email.toLocaleLowerCase();
+    var key = [name, phone, email].join(' ');
+    if (isPhoneValid(phone) && isEmailValid(lowerEmail)) {
+        phoneBook[key] = {name: name, phone: phone, email: email};
+    }
 };
 
-/*
-   Функция поиска записи в телефонную книгу.
-   Поиск ведется по всем полям.
-*/
 module.exports.find = function find(query) {
-
-    // Ваша удивительная магия здесь
-
+    if (query) {
+        Object.values(phoneBook).forEach(function (record) {
+            var found = false;
+            Object.keys(record).forEach(function (key) {
+                if (found) {
+                    return;
+                }
+                if (record[key].includes(query)) {
+                    console.log([record.name, record.phone, record.email].join(', '));
+                    found = true;
+                }
+            });
+        });
+    } else {
+        Object.values(phoneBook).forEach(function (record) {
+            console.log([record.name, record.phone, record.email].join(', '));
+        });
+    }
 };
 
-/*
-   Функция удаления записи в телефонной книге.
-*/
 module.exports.remove = function remove(query) {
-
-    // Ваша необьяснимая магия здесь
-
+    var removedContacts = 0;
+    Object.keys(phoneBook).forEach(function (recordKey) {
+        var removed = false;
+        var record = phoneBook[recordKey];
+        Object.keys(record).forEach(function (key) {
+            if (!removed && record[key].includes(query)) {
+                delete phoneBook[recordKey];
+                removedContacts++;
+            };
+        });
+    });
+    if (removedContacts === 1) {
+        console.log('Removed 1 contact');
+    } else {
+        if (removedContacts > 0) {
+            console.log('Removed ' + removedContacts + ' contacts');
+        } else {
+            console.log('No contact wasn\'t removed');
+        }
+    }
 };
 
-/*
-   Функция импорта записей из файла (задача со звёздочкой!).
-*/
 module.exports.importFromCsv = function importFromCsv(filename) {
     var data = require('fs').readFileSync(filename, 'utf-8');
-
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
+    data.split('\r\n').forEach(function (line) {
+        var splittedLine = line.split(';');
+        module.exports.add(splittedLine[0], splittedLine[1], splittedLine[2])
+    });
 };
 
-/*
-   Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
-*/
-module.exports.showTable = function showTable(filename) {
+String.prototype.repeat=function(n)
+// повторяет строку n раз
+{
+    var r='';
+    if (typeof(n)=='number'){
+        for (var i=1; i<=n; i++) {
+            r+=this;
+        }
+    }
+    return r;
+}
 
-    // Ваша чёрная магия здесь
+// String.prototype.padRight=function(n,c) {
+//     var r = '';
+//     var s = ' ';
+//     if (typeof(c)=='string') {
+//         if (c.length>0) {
+//             s=c;
+//         }
+//     }
+//     if (typeof(n) === 'number')
+//     {
+//         if (n > 0) {
+//             var l=this.length;
+//             if (l>n)
+//             r=this.substr(0,n);
+//             else
+//             if (l === n) {
+//                 r=this;
+//             } else {
+//                 r = this;
+//                 while (r.length < n) {
+//                     r+=s;
+//                 }
+//                 r = r.substr(0, n);
+//             }
+//         }
+//     }
+//     return r;
+// }
+
+// String.prototype.padLeft=function(n, c) {
+//     var r='';
+//     var s=' ';
+//     if (typeof(c) === 'string') {
+//         if (c.length > 0) {
+//             s = c;
+//         }
+//     }
+
+//     if (typeof(n) === 'number') {
+//         if (n > 0)
+//         {
+//             var l = this.length;
+//             if (l > n) {
+//                 r = this.substr(l - n);
+//             } else {
+//                 if (l == n) {
+//                     r = this;
+//                 } else {
+//                     r = this;
+//                     while (r.length < n) {
+//                         r = s + r;
+//                     }
+//                     r = r.substr(r.length - n);
+//                 }
+//             }
+//         }
+//     }
+//     return r;
+// }
+
+module.exports.showTable = function showTable(filename) {
+    var maxNameLength = 0;
+    var maxEmailLength = 0;
+    var maxPhoneLength = 0
+    for (var key in phoneBook) {
+        if (phoneBook[key].name.length > maxNameLength) {
+            maxNameLength = phoneBook[key].name.length;
+        }
+        if (phoneBook[key].email.length > maxEmailLength) {
+            maxEmailLength = phoneBook[key].email.length;
+        }
+        if (phoneBook[key].phone.length > maxPhoneLength) {
+            maxPhoneLength = phoneBook[key].phone.length;
+        }
+    }
+    console.log('┌' + '─'.repeat(maxNameLength) + '┬' + '─'.repeat(maxPhoneLength) + '╥' + '─'.repeat(maxEmailLength) + '┐');
+    console.log('│' + ' '.repeat(Math.ceil((maxNameLength - 'Name'.length) / 2)) +
+                'Name' + ' '.repeat(Math.round((maxNameLength - 'Name'.length) / 2)) + '│' + 
+                ' '.repeat(Math.ceil((maxPhoneLength - "Phone number".length) / 2)) + "Phone number" + 
+                ' '.repeat(Math.floor((maxPhoneLength - "Phone number".length) / 2)) + '║' + 
+                ' '.repeat(Math.ceil((maxEmailLength - 'Email'.length) / 2)) + 'Email' + 
+                ' '.repeat(Math.floor((maxEmailLength - 'Email'.length) / 2)) +
+                '│'
+                );
+    
+    Object.values(phoneBook).forEach(function (record) {
+        
+        console.log('├' + '─'.repeat(maxNameLength) + '┼' + '─'.repeat(maxPhoneLength) + '╫' + '─'.repeat(maxEmailLength) + '┤');
+        console.log(
+        '│' + ' '.repeat(Math.ceil((maxNameLength - record.name.length) / 2)) + record.name + 
+        ' '.repeat(Math.floor((maxNameLength - record.name.length) / 2)) + '│' + 
+        
+        ' '.repeat(Math.ceil((maxPhoneLength - record.phone.length) / 2)) + record.phone + 
+        ' '.repeat(Math.floor((maxPhoneLength - record.phone.length) / 2)) + '║' +
+        
+        ' '.repeat(Math.ceil((maxEmailLength - record.email.length) / 2)) + 
+        record.email + 
+        ' '.repeat(Math.floor((maxEmailLength - record.email.length) / 2)) + 
+        '│');
+    })
+    console.log('└' + '─'.repeat(maxNameLength) + '┴' + '─'.repeat(maxPhoneLength) + '╨' + '─'.repeat(maxEmailLength) + '┘');
 
 };
